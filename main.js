e836d19f56ee656b5f4ebe8b7836bb661fb1754d
keerthanakannan-7 <keerthanakannan3833@gmail.com>
2025-07-13 09:42:12 +0530

    route structure

--- main.js
+++ main.js
@@ -8,6 +8,31 @@ app.get('/', (req, res) => {
     res.json({msg: "Hello"});
 });
 
+
+// CRUD Functionality of Movies
+
+// R - For Reading movies
+app.get('/movies', () => {
+
+})
+
+// C - For creating movies
+app.post('/movies', () =>{
+
+})
+
+// U - For Update movie
+app.put('/movies/:id', () => {
+
+})
+
+// D - For Deleting movie
+app.delete('/movies/:id', () => {
+    
+})
+ 
+
+
 app.listen(PORT, () =>{
     console.log(`This server is running at http://localhost:${PORT}`)
 })
